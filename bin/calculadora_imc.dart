d4a0da57741da071e625341dfd4d48a2cd26de03
Wayne Santos <waynerosan@gmail.com>
2023-08-30 19:13:21 -0300

    Commit inicial

---
+++ bin/calculadora_imc.dart
@@ -0,0 +1,5 @@
+import 'package:calculadora_imc/calculadora_imc.dart' as calculadora_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadora_imc.calculate()}!');
+}
---
+++ test/calculadora_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculadora_imc/calculadora_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
